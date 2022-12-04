98e38da5a295bd66d8f84eb8e432f1cd7bec90b9
Vigribor <110626977+Vigribor@users.noreply.github.com>
2022-12-04 09:22:08 +0300

    Add files via upload

---
+++ javascript - функции/Задание 5 - Стрелочные функции.js
@@ -0,0 +1,13 @@
+// const arrow = (a) => {
+// console.log(a)
+//}
+
+const arrow = (a,b,c,d,e,f) => {
+    console.log(b)
+    console.log(a)
+    console.log(e)
+    console.log(d)
+    console.log(c)
+    console.log(f)
+}
+arrow(5,8,6,4,3,1)
